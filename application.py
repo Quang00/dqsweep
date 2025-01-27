2c48c49cbc335600277f57bbe9909325604368df
Quang00 <81580063+Quang00@users.noreply.github.com>
2025-01-27 14:00:40 +0100

    fix perfect config

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from netqasm.sdk.classical_communication.socket import Socket
 from netqasm.sdk.connection import BaseNetQASMConnection
 from netqasm.sdk.epr_socket import EPRSocket
@@ -7,6 +6,7 @@ from netqasm.sdk.qubit import Qubit
 from netsquid.qubits.dmutil import dm_fidelity
 from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
 from squidasm.util import get_qubit_state
+
 
 class AliceProgram(Program):
     PEER_NAME = "Bob"
@@ -42,7 +42,7 @@ class AliceProgram(Program):
 
             # Perfom a CNOT gate between alice qubit and her shared entangled qubit a1
             alice_qubit.cnot(a1_qubit)
-            
+
             # Alice measures her entangled qubit a1
             a1_measurement = a1_qubit.measure()
             yield from connection.flush()
@@ -52,7 +52,7 @@ class AliceProgram(Program):
 
             # Alice listens the classical channel to get Bob's measurement
             b1_measurement = yield from csocket.recv()
-            if b1_measurement == '1':
+            if b1_measurement == "1":
                 alice_qubit.Z()
 
             alice_qubit.free()
@@ -83,7 +83,7 @@ class BobProgram(Program):
         epr_socket: EPRSocket = context.epr_sockets[self.PEER_NAME]
         # get connection to quantum network processing unit
         connection: BaseNetQASMConnection = context.connection
-        
+
         fidelities = []
         for _ in range(self._num_epr_rounds):
             # Listen for request to create EPR pair
@@ -94,14 +94,14 @@ class BobProgram(Program):
             # Create a local qubit which is the target qubit of the distributed CNOT gate
             bob_qubit = Qubit(connection)
             bob_qubit.reset()
-        
+
             # Bob listens the classical channel to get the measurement from Alice
             a1_measurement = yield from csocket.recv()
 
             # At this stage a distributed CNOT was perfomed between Alice qubit and b1 qubit
-            if a1_measurement == '1':
+            if a1_measurement == "1":
                 b1_qubit.X()
-            
+
             b1_qubit.cnot(bob_qubit)
             b1_qubit.H()
             b1_measurement = b1_qubit.measure()
@@ -115,8 +115,8 @@ class BobProgram(Program):
             # Create the perfect resulting density matrix to assess the fidelity
             state_ref = np.array([0, 0, 0, 1], dtype=complex)
             dm_ref = np.outer(state_ref, np.conjugate(state_ref))
-        
-            # Compute the fidelity between the two density matrices    
+
+            # Compute the fidelity between the two density matrices
             fidelity = dm_fidelity(dm_b, dm_ref)
             fidelities.append(fidelity)
 
